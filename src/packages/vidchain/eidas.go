3b40bd9a7ffbc3e776fa0b82b2378b83f5c64e72
Mauro Lucchini <maurolucchinidepompa@gmail.com>
2023-04-20 15:44:35 +0200

    feat: eseal vc

--- src/packages/vidchain/eidas.go
+++ src/packages/vidchain/eidas.go
@@ -1,1 +1,31 @@
 package vidchain
+
+import (
+	"bytes"
+	"github.com/validatedid/trussihealth-api/src/packages/restClient"
+	"io"
+	"net/http"
+)
+
+type Eidas struct {
+	httpClient    restClient.HTTPClient
+	authenticator Authenticator
+}
+
+type EsealedVerifiableCredential struct {
+	Content []byte
+}
+
+func NewEidas(client restClient.HTTPClient, apiAuthenticator Authenticator) (e *Eidas) {
+	return &Eidas{httpClient: client, authenticator: apiAuthenticator}
+}
+
+func (e Eidas) EsealVc(payload VerifiableCredential) (esealedVerifiableCredential EsealedVerifiableCredential) {
+	accessToken := e.authenticator.GetAccessToken()
+	request, _ := http.NewRequest("POST", "https://dev.vidchain.net/api/v1/eidas/signatures", bytes.NewBuffer(payload.Content))
+	request.Header.Set("Authorization", "Bearer "+accessToken)
+	response, _ := e.httpClient.Do(request)
+	body, _ := io.ReadAll(response.Body)
+	esealedVerifiableCredential.Content = body
+	return esealedVerifiableCredential
+}
