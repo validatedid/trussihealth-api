accaf1035164c0a455a572820c038c840b05aed5
Marcos <marcos.validatedid.com>
2023-09-04 15:14:24 +0200

    Add httpClientMock

---
+++ tests/packages/issuer/httpClientMock_test.go
@@ -0,0 +1,20 @@
+package vidchain_test
+
+import (
+	"net/http"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type httpClientMock struct {
+	mock.Mock
+}
+
+func newHttpClientMock() *httpClientMock {
+	return &httpClientMock{}
+}
+
+func (m *httpClientMock) Do(req *http.Request) (*http.Response, error) {
+	args := m.Called(req)
+	return args.Get(0).(*http.Response), args.Error(1)
+}
